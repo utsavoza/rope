1b5f9bc2354807106f019fe96ee03b4fd1f45644
utsavoza <utsavoza96@gmail.com>
2018-03-05 22:53:58 +0530

    Initial commit

---
+++ rope/src/main/java/com/utsavoza/rope/Rope.java
@@ -0,0 +1,4 @@
+package com.utsavoza.rope;
+
+public interface Rope {
+}
---
+++ rope/src/test/java/com/utsavoza/rope/RopeTest.java
@@ -0,0 +1,12 @@
+package com.utsavoza.rope;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class RopeTest {
+
+  @Test public void todoTest() {
+    assertTrue("TODO: Implement", false);
+  }
+}
