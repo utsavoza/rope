8b9cfa12e8374f6fd68c08ce2b837db9a0d00346
utsavoza <utsavoza96@gmail.com>
2018-03-08 00:16:19 +0530

    Initial API

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Rope
 ====
+A *rope* is a tree-like data structure that provides a more efficient way of concatenating strings, 
+with each internal node representing the concatenation of its children, and the leaves consisting of
+flat strings, usually represented as contiguous array of characters.
 
 License
 -------
--- rope/src/main/java/com/utsavoza/rope/Rope.java
+++ rope/src/main/java/com/utsavoza/rope/Rope.java
@@ -1,4 +1,22 @@
 package com.utsavoza.rope;
 
-public interface Rope {
+/**
+ * <p>A rope is a tree-like data structure that provides a more efficient way
+ * of concatenating strings, with each internal node representing the concatenation
+ * of its children, and the leaves consisting of flat strings, usually represented
+ * as contiguous array of characters.</p>
+ */
+public interface Rope extends CharSequence, Comparable<Rope>, Iterable<Character> {
+
+  Rope insert(int offset, CharSequence cs);
+
+  Rope append(CharSequence suffix);
+
+  Rope delete(int beginIndex, int endIndex);
+
+  Rope rebalance();
+
+  Rope subSequence(int beginIndex, int endIndex);
+
+  boolean isEmpty();
 }
