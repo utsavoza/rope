2e454f75afeb301cb0459767dc84edc9cd06085c
utsavoza <utsavoza96@gmail.com>
2018-03-13 00:28:35 +0530

    Update build.gradle

--- rope/src/test/java/com/utsavoza/rope/RopeTest.java
+++ rope/src/test/java/com/utsavoza/rope/RopeTest.java
@@ -1,12 +1,13 @@
 package com.utsavoza.rope;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
 public class RopeTest {
 
-  @Test public void todoTest() {
+  @Ignore @Test public void todoTest() {
     assertTrue("TODO: Implement", false);
   }
 }
